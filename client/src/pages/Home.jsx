2188b02ed108e2d5738c572f4ae9310536228632
Anthony Gress <the.techx.guy@gmail.com>
2021-08-30 17:44:00 -0400

    adjusted splash image position

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,18 +3,17 @@ import Cart from "../components/Cart";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import heroImg from "../assets/images/chefDrizzle.jpeg";
 
 const Home = () => {
   return (
     <section id="home">
+      <Box className="heroImg"></Box>
       <Container>
         <Cart />
         <Typography align="center" variant="h3">
           {"Home"}
         </Typography>
       </Container>
-      <Box className="heroImg"></Box>
     </section>
   );
 };
