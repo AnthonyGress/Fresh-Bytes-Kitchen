ba429f79847ed17cc1f56fea5594e52e68aa5ace
Anthony Gress <the.techx.guy@gmail.com>
2021-08-31 14:13:00 -0400

    housekeeping

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -192,27 +192,6 @@ export default function CartDrawer() {
             {"Cart is empty"}
           </Typography>
         )}
-        {/* <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List> */}
       </Drawer>
     </>
   );
