88103e1536b6c8205385d12aa05104d571c63b6f
Anthony Gress <the.techx.guy@gmail.com>
2021-08-31 13:19:49 -0400

    timestamps on users and products

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER } from "../utils/queries";
 
 const Account = () => {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+
+  if (data) {
+    user = data.user;
+  }
+  console.log(user);
   return (
     <section id="account">
       <Container>
         <Typography align="center" variant="h3">
           {"My Account"}
         </Typography>
+        <Box></Box>
       </Container>
     </section>
   );
