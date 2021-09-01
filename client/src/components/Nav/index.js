21a57c75b4ae6699f7ab0e54cf7c830f153c9121
Anthony Gress <the.techx.guy@gmail.com>
2021-09-01 00:00:38 -0400

    make hamburger menu larger

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -88,7 +88,7 @@ export default function MenuAppBar() {
             color="inherit"
             aria-label="menu"
           >
-            <MenuIcon />
+            <MenuIcon fontSize="large" />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             FBK
