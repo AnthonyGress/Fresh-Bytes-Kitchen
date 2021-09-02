b4c29c40b391fea2e65979f1621862580e430158
Casey R <crowlandsdev@gmail.com>
2021-09-02 14:25:02 -0400

    changed background of expanded menu, changed menu font, and expanded background to cover page when it expands

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -15,18 +15,21 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Container } from "@material-ui/core";
+import Wood from '../../assets/images/black-wood.jpeg';
+
 
 const useStyles = makeStyles((theme) => ({
   catOption: {
     backgroundColor: "#c82427",
     borderRadius: '30px 30px 30px 30px'
   },
-  catOptionDetails: {
-    backgroundColor: "#ffcd27",
+  catOptionProducts: {
+     backgroundImage: `url(${Wood})`
   },
   catOptionTitle: {
     color: "#ffcd27",
-    fontSize: "2em"
+    fontSize: "2em",
+    fontFamily: "'Courgette', cursive"
   },
 }));
 
@@ -147,30 +150,28 @@ function CategoryMenu() {
     <div>
       <Container maxWidth="lg" style={{marginTop: "5rem"}}>
         {categories.map((item) => (
-          <Accordion
-            expanded={expanded === item.name}
-            onChange={handleChange(item.name)}
-            key={item._id}
-            className={classes.catOption}
-            onClick={() => {
-              handleClick(item._id);
-            }}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1bh-content"
-              id={item.name}
+            <Accordion
+              expanded={expanded === item.name}
+              onChange={handleChange(item.name)}
+              key={item._id}
+              className={classes.catOption}
+              onClick={() => {
+                handleClick(item._id);
+              }}
             >
-              <Typography className={classes.catOptionTitle}>
-                {item.name}
-              </Typography>
-            </AccordionSummary>
-              <AccordionDetails className={classes.catOptionDetails}>
-                <Typography>
-                  <ProductList />
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id={item.name}
+              >
+                <Typography className={classes.catOptionTitle}>
+                  {item.name}
                 </Typography>
-              </AccordionDetails>
-          </Accordion>
+              </AccordionSummary>
+                <AccordionDetails className={classes.catOptionProducts}>
+                    <ProductList />
+                </AccordionDetails>
+            </Accordion>
         ))}
       </Container>
     </div>
--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -2,12 +2,13 @@ import CategoryMenu from "../components/CategoryMenu";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
+const font = "'Courgette', cursive";
 
 const Menu = () => {
   return (
     <section id="menu">
       <Container>
-        <Typography align="center" variant="h1" style={{paddingTop: "8rem"}}>
+        <Typography align="center" variant="h1" style={{paddingTop: "8rem", fontFamily: font}}> 
           {"Menu"}
         </Typography>
       </Container>
